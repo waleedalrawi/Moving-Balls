87ba9b570b507072e953455e84faba89879d148f
waleedalrawi <62625606+waleedalrawi@users.noreply.github.com>
2022-03-13 23:40:58 -0700

    adjusted border detection to use correct end of the ball, added velocity adjustment at border, moved height and width style attributes inline in the html file, adjusted README

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ This is a rather elementary implementation of simulated balls moving on the scre
 ## Usage
 
 Load the index.html page and enjoy :)
+Balls change direction when hitting a border as well as get a random new size, color, and speed!
+
+## Roadmap / questions
+
+Some css had to be added inline in order for those attributes to be accessed by JS. Need to figure out how to access those attributes even when they are placed in the separate css style sheet. (Specifically referring to the first access to width and height attributes of ball.style, before those values are overwritten in JS. After they've been overwritten once in JS there are no issues).
 
 ## License
 
--- ball.js
+++ ball.js
@@ -23,6 +23,16 @@ const Xmax = 900;
 const Ymin = 0;
 const Ymax = 600;
 
+//driver; calls setinterval on all the ball objects
+{var i = 0;
+  while(i < num_balls){
+    setInterval(moveBall, 40, balls[i], positionX, positionY, velocityX, velocityY, reverseX, reverseY,i);
+    i++
+  }
+ }
+ 
+ 
+ //TODO: CHANGE SIZE/ SPEED, ADD MORE BALLS, DETECTION BETWEEN BALLS
 
 
 //function to generate random color
@@ -39,8 +49,7 @@ function randomColor(){
 //this is disugsting. arrays are passed to get pass by reference functionality, and the index telling you which
 //array element to manipulate is passed along
 function moveBall(ball, positionX, positionY, velocityX, velocityY, reverseX, reverseY, i) {
-  console.log("move function called on ", ball, positionX, positionY)
-//update positions
+  //update positions
   if(!reverseX[i]){
     positionX[i] += velocityX[i];
   }
@@ -60,48 +69,78 @@ function moveBall(ball, positionX, positionY, velocityX, velocityY, reverseX, re
   ball.style.top = positionY[i] + "px";
 
 //detect edges and apply desired effects if edge detected
-  if(positionX[i] >= Xmax || positionX[i] <= Xmin){
+  if(positionX[i] + parseInt(ball.style.width) >= window.innerWidth){
     //change reverse variable used by move function to know to change direction
     reverseX[i] = !reverseX[i];
     //change to a random color when you hit border
     ball.style.background = randomColor();
     //change to a random size when you hit border
-    let new_size = Math.floor(25 + 100*Math.random())
+    let new_size = Math.floor(25 + 100*Math.random());
     ball.style.width = new_size + "px";
     ball.style.height = new_size + "px";
-    /*
+    positionX[i] = window.innerWidth - new_size - 1; //1 away from the right border (because condition has >=)
+    ball.style.left = positionX[i] + "px";
+    
     //change to a random speed when you hit border
     let new_velocity = Math.floor(5+15*Math.random());
-    velocityX = new_velocity;
-    velocityY = new_velocity;
-    */
+    velocityX[i] = new_velocity;
+    velocityY[i] = new_velocity;
+    
   }
-  if(positionY[i] >= Ymax || positionY[i] <= Ymin){
+  if(positionX[i] <= Xmin){
+    //change reverse variable used by move function to know to change direction
+    reverseX[i] = !reverseX[i];
+    //change to a random color when you hit border
+    ball.style.background = randomColor();
+    //change to a random size when you hit border
+    let new_size = Math.floor(25 + 100*Math.random());
+    ball.style.width = new_size + "px";
+    ball.style.height = new_size + "px";
+    positionX[i] = 1; //1 away from the left border (because condition has <=)
+    ball.style.left = positionX[i] + "px";
+    
+    //change to a random speed when you hit border
+    let new_velocity = Math.floor(5+15*Math.random());
+    velocityX[i] = new_velocity;
+    velocityY[i] = new_velocity;
+    
+  }
+  if(positionY[i] + parseInt(ball.style.height) >= window.innerHeight){
     //change reverse variable used by move function to know to change direction
     reverseY[i] = !reverseY[i];
     //change to a random color when you hit border
     ball.style.background = randomColor();
     //change to a random size when you hit border
-    let new_size = Math.floor(25 + 100*Math.random())
+    let new_size = Math.floor(25 + 100*Math.random());
     ball.style.width = new_size + "px";
     ball.style.height = new_size + "px";
-    /*
+    positionY[i] = window.innerHeight - new_size - 1; //1 away from the bottom border (because condition has <=)
+    ball.style.top = positionY[i] + "px";
+    
     //change to a random speed when you hit border
     let new_velocity = Math.floor(5+15*Math.random());
-    velocityX = new_velocity;
-    velocityY = new_velocity;
-    */
+    velocityX[i] = new_velocity;
+    velocityY[i] = new_velocity;
+    
+  }
+  if(positionY[i] + parseInt(ball.style.height) >= window.innerHeight || positionY[i] <= Ymin){
+    //change reverse variable used by move function to know to change direction
+    reverseY[i] = !reverseY[i];
+    //change to a random color when you hit border
+    ball.style.background = randomColor();
+    //change to a random size when you hit border
+    let new_size = Math.floor(25 + 100*Math.random());
+    ball.style.width = new_size + "px";
+    ball.style.height = new_size + "px";
+    positionY[i] = 1; //1 away from the bottom border (because condition has <=)
+    ball.style.top = positionY[i] + "px";
+    
+    //change to a random speed when you hit border
+    let new_velocity = Math.floor(5+15*Math.random());
+    velocityX[i] = new_velocity;
+    velocityY[i] = new_velocity;
+    
   }
 
   
 }
-
-//driver; calls setinterval on all the ball objects
-{var i = 0;
- while(i < num_balls){
-   setInterval(moveBall, 40, balls[i], positionX, positionY, velocityX, velocityY, reverseX, reverseY,i);
-   i++
- }
-}
-
-//TODO: CHANGE SIZE/ SPEED, ADD MORE BALLS, DETECTION BETWEEN BALLS
